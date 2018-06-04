ebbf1cd971ab9326dfbfacc739b0a89768686048
ThanhTuDT <35495120+ThanhTuDT@users.noreply.github.com>
2018-06-04 22:31:23 +0700

    sql

---
+++ Proc_SP.sql
@@ -0,0 +1,9 @@
+﻿create proc XemSP 
+as
+begin
+select * from tbl_sanpham
+end
+go
+
+create proc ThemSP(@id nchar(10), @id_KM chả(10), @id_DM nchar(10), @ten nvarchar(50), @linkanh nvarchar(50), @gia int,
+@SoLg int, @trongLg int, @ROM int, @RAM int, @thenho int, @CamTrc int, @CamSau int, @pin int, @
